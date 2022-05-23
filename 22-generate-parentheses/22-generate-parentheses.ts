f99269c5507ac6dc064410973b8bb45980432e09
amit-biswas-1992 <78925979+amit-biswas-1992@users.noreply.github.com>
2022-05-23 21:47:08 +0600

    Time: 95 ms (53.47%), Space: 44.5 MB (89.72%) - LeetHub

--- 22-generate-parentheses/22-generate-parentheses.ts
+++ 22-generate-parentheses/22-generate-parentheses.ts
@@ -2,6 +2,7 @@ function generateParenthesis(n: number): string[] {
     
     let result : string[] = []
     
+    
     generate(n,0,0,"",result)
     
     return result
@@ -11,15 +12,15 @@ function generateParenthesis(n: number): string[] {
 
 function generate(n: number,leftCount: number,rightCount: number,currString: string,result: string[]): void{
     
-    if(leftCount<rightCount || leftCount>n || rightCount>n) return
+    if(leftCount<rightCount) return
     
     if(currString.length == 2*n){
         result.push(currString)
     }
     
 
-    generate(n,leftCount+1,rightCount,currString+"(",result)
-    generate(n,leftCount,rightCount+1,currString+")",result)
+    if(leftCount<n) generate(n,leftCount+1,rightCount,currString+"(",result)
+    if(rightCount<leftCount) generate(n,leftCount,rightCount+1,currString+")",result)
 
     return
     
