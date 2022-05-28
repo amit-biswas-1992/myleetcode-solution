3d25f9651249ba33d2849b37afffe5e8890d26d8
amit-biswas-1992 <78925979+amit-biswas-1992@users.noreply.github.com>
2022-05-28 23:12:51 +0600

    Time: 322 ms (10.78%), Space: 54.5 MB (41.54%) - LeetHub

---
+++ 49-group-anagrams/49-group-anagrams.ts
@@ -0,0 +1,32 @@
+/**
+ * @param {string[]} strs
+ * @return {string[][]}
+ */
+var groupAnagrams = function(strs) {
+    let map: Map<string, string[]> = new Map()
+    
+    for(const str of strs){
+        let ca = Array(26).fill(0)
+        
+        for(let i=0;i<str.length;i++){
+            ca[str[i].charCodeAt(0)-97]++
+            
+        }
+        
+        
+        let countStr = ca.join()
+        
+        console.log(countStr)
+        
+        if(map.has(countStr)){
+            map.get(countStr).push(str)
+        }
+        else{
+            map.set(countStr,[str])
+        }
+        
+        
+    }
+    
+    return Array.from(map.values())
+};
