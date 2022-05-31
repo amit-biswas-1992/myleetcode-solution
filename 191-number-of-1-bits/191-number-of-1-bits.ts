e37f23688bc6ba923c0f7942aa72a5568aac9e1b
amit-biswas-1992 <78925979+amit-biswas-1992@users.noreply.github.com>
2022-05-31 13:15:07 +0600

    Time: 143 ms (6.67%), Space: 44.3 MB (85.67%) - LeetHub

--- 191-number-of-1-bits/191-number-of-1-bits.ts
+++ 191-number-of-1-bits/191-number-of-1-bits.ts
@@ -1,11 +1,4 @@
 function hammingWeight(n: number): number {
     
-    let countBit = 0
-    
-    while(n!=0){
-        countBit += n&1
-        n = n >>> 1
-    }
-    
-    return countBit
+    return n.toString(2).split("").filter( n => n =='1').length
 };
