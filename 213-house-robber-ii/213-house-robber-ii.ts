b3b7e17dc79ee5f1da50cba903d1a3968248d9fc
amit-biswas-1992 <78925979+amit-biswas-1992@users.noreply.github.com>
2022-06-08 01:25:23 +0600

    Time: 62 ms (96.60%), Space: 43.5 MB (34.69%) - LeetHub

---
+++ 213-house-robber-ii/213-house-robber-ii.ts
@@ -0,0 +1,22 @@
+function rob(nums: number[]): number {
+    if(nums.length == 1) return nums[0]
+    let map = new Map()
+    let map2 = new Map()
+    return Math.max(helper(nums,1,nums.length-1,map),helper(nums,0,nums.length-2,map2))
+};
+
+
+function helper(nums: number[], index: number,lastIndex: number, map: Map<number,number>){
+    if(index>lastIndex) return 0
+
+    if(map.has(index)) return map.get(index)
+    
+    let included = nums[index] + helper(nums,index+2,lastIndex,map)
+    let excluded = helper(nums,index+1,lastIndex,map)
+    
+    let max = Math.max(included,excluded)
+    
+    map.set(index,max)
+    
+    return max
+}
