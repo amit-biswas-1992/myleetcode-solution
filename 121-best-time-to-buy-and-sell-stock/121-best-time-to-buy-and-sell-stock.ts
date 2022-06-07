0722595b800be32b43306c798db04c7316c61dc2
amit-biswas-1992 <78925979+amit-biswas-1992@users.noreply.github.com>
2022-06-08 00:23:08 +0600

    Time: 153 ms (22.11%), Space: 52.2 MB (27.56%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.ts
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.ts
@@ -1,11 +1,15 @@
 function maxProfit(prices: number[]): number {
     let minPrice = Infinity
     let maxProfit = 0
+    let left = 0
     
     
-    for(let i=0;i<prices.length;i++){
-        maxProfit = Math.max(maxProfit,prices[i]-minPrice)
-        minPrice = Math.min(minPrice,prices[i])
+    for(let i=1;i<prices.length;i++){
+        
+        if(prices[left]>prices[i]) left = i
+        else{
+            maxProfit = Math.max(maxProfit,prices[i]-prices[left])
+        }
     }
     
     return maxProfit
