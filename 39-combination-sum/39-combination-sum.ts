2e27b6c8d4e897865071670fb6ef8182be369aa6
amit-biswas-1992 <78925979+amit-biswas-1992@users.noreply.github.com>
2022-06-07 14:45:43 +0600

    Time: 158 ms (17.95%), Space: 46.6 MB (49.45%) - LeetHub

--- 39-combination-sum/39-combination-sum.ts
+++ 39-combination-sum/39-combination-sum.ts
@@ -9,21 +9,20 @@ function combinationSum(candidates: number[], target: number): number[][] {
 
 function helper(candidates: number[], target: number, index: number, curSum: number,curr: number[],result: number[][]){
     
-    if(curSum>target) return
+    if(curSum>target || index>candidates.length-1) return
     
     if(curSum==target){
         result.push(Array.from(curr))
         return
     } 
     
-    for(let i=index;i<candidates.length;i++){
-        curr.push(candidates[i])
-        curSum+=candidates[i]
-        helper(candidates,target,i,curSum,curr,result)
-        curSum-=candidates[i]
+        curr.push(candidates[index])
+        helper(candidates,target,index,curSum+candidates[index],curr,result)
         curr.pop()
+        helper(candidates,target,index+1,curSum,curr,result)
         
-    }
+        
+    
     
     
     
