b1b7bb04abdcdb2c7b8ba57d08340cd972de584e
amit-biswas-1992 <78925979+amit-biswas-1992@users.noreply.github.com>
2022-06-10 01:46:32 +0600

    Time: 9881 ms (5.17%), Space: 56.3 MB (5.17%) - LeetHub

--- 55-jump-game/55-jump-game.ts
+++ 55-jump-game/55-jump-game.ts
@@ -5,19 +5,22 @@ function canJump(nums: number[]): boolean {
 
 
 function helper(nums: number[], step: number, set: Set<number>) : boolean {
-    
+    //return if step equal to destination final index
     if(step == nums.length-1){
         return true
     } 
     
+    //return if step becomes higher than destination
     if(step>nums.length-1) return false
     
+    //same step has been processed again, that means it is false. If true, then it would not have been processed multiple times.
     if(set.has(step)) return false
     
     set.add(step)
     
     let res = false
     
+    // step = maximum step we can take. so we need to check all possible step from 1 to current step value
     for(let i=1;i<=nums[step];i++){
         res = res || helper(nums,step+i,set)
        
