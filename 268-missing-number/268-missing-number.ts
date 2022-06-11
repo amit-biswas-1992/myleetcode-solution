0c2c5055b81a03bc602d68a08c22fbe99abc7920
amit-biswas-1992 <78925979+amit-biswas-1992@users.noreply.github.com>
2022-06-11 18:05:07 +0600

    Time: 110 ms (49.62%), Space: 45.1 MB (76.86%) - LeetHub

---
+++ 268-missing-number/268-missing-number.ts
@@ -0,0 +1,9 @@
+function missingNumber(nums: number[]): number {
+    let missing = nums.length
+    
+    for(let i=0;i<nums.length;i++){
+        missing ^= i^nums[i]
+    }
+    
+    return missing
+};
