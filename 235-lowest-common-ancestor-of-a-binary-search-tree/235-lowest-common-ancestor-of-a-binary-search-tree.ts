42040d7a070814772731beda01268a7dce0201f6
amit-biswas-1992 <78925979+amit-biswas-1992@users.noreply.github.com>
2022-06-11 19:31:46 +0600

    Time: 120 ms (53.26%), Space: 53.5 MB (11.49%) - LeetHub

--- 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.ts
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.ts
@@ -13,15 +13,14 @@
  */
 
 function lowestCommonAncestor(root: TreeNode | null, p: TreeNode | null, q: TreeNode | null): TreeNode | null {
-    
-    if(root.val>p.val && root.val>q.val) return lowestCommonAncestor(root.left,p,q)
-    else if(root.val<p.val && root.val<q.val) return lowestCommonAncestor(root.right,p,q)
+	
+    if(root.val<p.val && root.val<q.val){
+        return lowestCommonAncestor(root.right,p,q)
+    }
+    if(root.val>p.val && root.val>q.val){
+        return lowestCommonAncestor(root.left,p,q)
+    }
     else{
         return root
     }
-    
-    
-	
 };
-
-
