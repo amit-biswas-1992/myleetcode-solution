42a9e2f0fad01b63349264afd314ce9f2a423a94
amit-biswas-1992 <78925979+amit-biswas-1992@users.noreply.github.com>
2022-06-18 13:44:00 +0600

    Time: 307 ms (21.10%), Space: 44.3 MB (91.56%) - LeetHub

---
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.ts
@@ -0,0 +1,23 @@
+function lengthOfLIS(nums: number[]): number {
+    
+    let dp = Array(nums.length).fill(1)
+    
+    
+    
+    for(let i=0;i<nums.length;i++){
+        for(let j=0;j<i;j++){
+            if(nums[i]>nums[j]){
+                dp[i] = Math.max(dp[i],dp[j]+1)
+            
+            }
+            
+        }
+    }
+
+    
+    return Math.max(...dp)
+    
+   
+};
+
+
