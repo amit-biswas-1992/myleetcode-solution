e6c163ec17832d05c14896848974249e4840124c
amit-biswas-1992 <78925979+amit-biswas-1992@users.noreply.github.com>
2022-06-20 01:34:04 +0600

    Time: 374 ms (49.50%), Space: 76 MB (53.12%) - LeetHub

--- 435-non-overlapping-intervals/435-non-overlapping-intervals.ts
+++ 435-non-overlapping-intervals/435-non-overlapping-intervals.ts
@@ -2,7 +2,6 @@ function eraseOverlapIntervals(intervals: number[][]): number {
     
     intervals.sort((a,b) => a[1]-b[1])
     
-    console.log(intervals)
     
     let current = 0    
     let removeCount = 0
