c22149fd164abc9385a2670897b5eb631327dda4
amit-biswas-1992 <78925979+amit-biswas-1992@users.noreply.github.com>
2022-06-21 23:02:12 +0600

    Time: 122 ms (26.06%), Space: 45.4 MB (19.72%) - LeetHub

--- 190-reverse-bits/190-reverse-bits.ts
+++ 190-reverse-bits/190-reverse-bits.ts
@@ -1,11 +1,12 @@
 function reverseBits(n: number): number {
-    let res = ""
+    let res = 0
 	for(let i=0;i<32;i++){
         let bit = n & 1
         
-        n = n >> 1
-        res += bit 
+        n = n >>> 1
+        res = res << 1
+        res |= bit
     }
     
-    return parseInt(res,2)
+    return res >>> 0
 };
