c39a0188514211ee0e8c027b056e4daf18dc8bf3
amit-biswas-1992 <78925979+amit-biswas-1992@users.noreply.github.com>
2022-07-01 12:01:45 +0600

    Time: 108 ms (69.72%), Space: 51.4 MB (53.67%) - LeetHub

---
+++ 11-container-with-most-water/11-container-with-most-water.ts
@@ -0,0 +1,20 @@
+function maxArea(height: number[]): number {
+    
+    let max = -Infinity
+    
+    let left = 0;
+    let right = height.length-1
+    
+    while(left<right){
+        max = Math.max(max, Math.min(height[left],height[right])*(right-left))
+        
+        if(height[left]<height[right]){
+            left++
+        }
+        else{
+            right--
+        }
+    }
+    
+    return max
+};
