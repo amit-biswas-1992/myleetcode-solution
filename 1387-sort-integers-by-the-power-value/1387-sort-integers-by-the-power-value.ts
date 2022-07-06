e88dc469b1938df90cbd05b51ac4b633539c7059
amit-biswas-1992 <78925979+amit-biswas-1992@users.noreply.github.com>
2022-07-06 21:42:47 +0600

    Time: 321 ms (33.33%), Space: 51.9 MB (11.11%) - LeetHub

---
+++ 1387-sort-integers-by-the-power-value/1387-sort-integers-by-the-power-value.ts
@@ -0,0 +1,31 @@
+function getKth(lo: number, hi: number, k: number): number {
+    let powerMap = new Map<number,number>()
+    
+    for(let i=lo;i<=hi;i++){
+        let steps = stepsToTranfromOne(i)
+        powerMap.set(i,steps)
+        
+    }
+    
+    let powers = [...powerMap.entries()].sort((a,b) => a[1]-b[1])
+    
+    console.log(powers)
+    
+    return powers[k-1][0]
+};
+
+
+function stepsToTranfromOne(x: number){
+    let steps = 0
+    while(x!=1){
+        if(x%2==1){
+            x = 3*x + 1
+        }
+        else{
+            x = Math.floor(x / 2)
+        }
+        steps++
+    }
+    
+    return steps
+}
