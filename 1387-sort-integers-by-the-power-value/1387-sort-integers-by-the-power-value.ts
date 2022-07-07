6167974d5b170345d3b684b9e155db4e270d5d99
amit-biswas-1992 <78925979+amit-biswas-1992@users.noreply.github.com>
2022-07-07 10:15:32 +0600

    Time: 207 ms (33.33%), Space: 49.4 MB (77.78%) - LeetHub

--- 1387-sort-integers-by-the-power-value/1387-sort-integers-by-the-power-value.ts
+++ 1387-sort-integers-by-the-power-value/1387-sort-integers-by-the-power-value.ts
@@ -1,30 +1,36 @@
 function getKth(lo: number, hi: number, k: number): number {
+    let powers = []
     let powerMap = new Map<number,number>()
     
     for(let i=lo;i<=hi;i++){
-        let steps = stepsToTranfromOne(i)
-        powerMap.set(i,steps)
+        let steps = findPowerVal(i,powerMap)
+        powers.push([i,steps])
         
     }
     
-    let powers = [...powerMap.entries()].sort((a,b) => a[1]-b[1])
+    powers = powers.sort((a,b) => a[1]-b[1])
     
     
     return powers[k-1][0]
 };
 
 
-function stepsToTranfromOne(x: number){
-    let steps = 0
-    while(x!=1){
-        if(x%2==1){
-            x = 3*x + 1
-        }
-        else{
-            x = Math.floor(x / 2)
-        }
-        steps++
-    }
+function findPowerVal(x: number, map: Map<number,number>){
     
+    if(x == 1) return 0
+    
+    if(map.has(x)) return map.get(x)
+    
+    let steps = 0
+    
+    if(x%2==1){
+        steps = findPowerVal(3*x+1,map) + 1
+    }
+    else{
+        steps = findPowerVal(Math.floor(x/2),map) + 1
+    }
+        
+    map.set(x,steps)
+   
     return steps
 }
