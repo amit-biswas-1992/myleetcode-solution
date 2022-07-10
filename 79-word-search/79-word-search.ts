d4b0c0678912dc9e53af438eb1428e6c5b91c8d3
amit-biswas-1992 <78925979+amit-biswas-1992@users.noreply.github.com>
2022-07-10 22:35:03 +0600

    Time: 327 ms (97.85%), Space: 43.2 MB (92.47%) - LeetHub

---
+++ 79-word-search/79-word-search.ts
@@ -0,0 +1,28 @@
+function exist(board: string[][], word: string): boolean {
+    let visited = new Array(board.length).fill(false).map( () => new Array(board[0].length).fill(false))
+    
+    for(let i=0;i<board.length;i++){
+        for(let j=0;j<board[0].length;j++){
+            if(board[i][j]==word[0] && wordSearch(board,word,0,i,j,visited)){
+                return true
+            }
+        }
+    }
+    
+    return false
+};
+
+function wordSearch(board: string[][], word: string, index: number, i: number, j: number,  visited: boolean[][]){
+    if(i<0 || j<0 || i>=board.length || j>=board[0].length || visited[i][j] || board[i][j]!=word[index]) return false
+    
+    visited[i][j] = true
+    
+    if(index == word.length-1 ) return true
+    
+    let found = wordSearch(board,word,index+1, i+1,j,visited) || wordSearch(board,word,index+1,i-1,j,visited) || wordSearch(board,word,index+1,i,j+1,visited) || wordSearch(board,word,index+1,i,j-1,visited)
+    
+    visited[i][j] = false
+    
+    return found
+
+}
