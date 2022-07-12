7359864fba7c8910b6008d004aef2183cea0e964
amit-biswas-1992 <78925979+amit-biswas-1992@users.noreply.github.com>
2022-07-12 23:53:18 +0600

    Time: 632 ms (11.11%), Space: 44.8 MB (64.73%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.ts
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.ts
@@ -1,23 +1,25 @@
 function lengthOfLIS(nums: number[]): number {
-    
-    let dp = Array(nums.length).fill(1)
-    
-    
-    
-    for(let i=0;i<nums.length;i++){
-        for(let j=0;j<i;j++){
-            if(nums[i]>nums[j]){
-                dp[i] = Math.max(dp[i],dp[j]+1)
-            
-            }
-            
-        }
-    }
-
-    
-    return Math.max(...dp)
-    
-   
+    let dp = Array(nums.length).fill(0)
+    return LIS(nums,-1,0,dp)
 };
 
+function LIS(nums: number[],prev: number, index: number, dp: number[]){
+    //console.log("prev: " + prev + " index: "+ index)
 
+    if(index==nums.length) return 0
+    
+    let taken = 0
+    if(prev==-1 || (nums[index]>nums[prev])){
+        if(dp[index]==0)
+            dp[index] = 1 + LIS(nums,index,index+1,dp)
+        
+    } 
+    taken = dp[index]
+    
+    
+    let notTaken = LIS(nums,prev,index+1,dp)
+    
+    
+    return Math.max(notTaken,taken)
+    
+}
