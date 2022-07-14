02fe94932e6a4f1baae03a9842e73b13ad2c1135
amit-biswas-1992 <78925979+amit-biswas-1992@users.noreply.github.com>
2022-07-14 09:36:00 +0600

    Time: 1051 ms (14.39%), Space: 149.8 MB (11.64%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.ts
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.ts
@@ -1,31 +1,28 @@
+//OC = o(m*n) , m = len1, n = len2
+//for any i,j => value could be re-calculated. so we need to store i,j either on dp[i][j] or on map
+//(0,0) => (1,1) by matching or (0,1) / (1,0) => (1,1) by skipping. so overlapping subproblem
+//i=11,j=2 so key=112 is confusing, it would either be formed by 11,2 or 1,12. so in between i and j, we need to put splitter like comma(,)
 function longestCommonSubsequence(text1: string, text2: string): number {
-    let map = new Map<string,number>()
-    return helper(text1,text2,0,0,map)
+    let map = new Map()
+    return lcs(text1,text2,0,0,map)
 };
 
-function helper(text1: string, text2: string, i: number, j: number, map: Map<string,number>){
+function lcs(text1: string,text2: string, index1: number, index2: number, map: Map<string,number>){
+    if(index1==text1.length || index2==text2.length) return 0
     
-    if(i>=text1.length || j>=text2.length) return 0;
-    let key = i+","+j
-    
+    let key = index1+","+index2
     if(map.has(key)) return map.get(key)
     
-    let max = 0
-    
-    if(text1[i]==text2[j]){
-        max += 1 + helper(text1,text2,i+1,j+1,map)
-        
-        
-    } else{
-        let exclude1 = helper(text1,text2,i+1,j,map)
-        let exclude2 = helper(text1,text2,i,j+1,map)
-        max += Math.max(exclude1,exclude2)
+    let maxLen = 0
+    if(text1[index1]==text2[index2]){
+        maxLen = 1 + lcs(text1,text2,index1+1,index2+1,map)
     }
-
-    map.set(key,max)
+    else{
+        maxLen = Math.max(lcs(text1,text2,index1+1,index2,map),lcs(text1,text2,index1,index2+1,map))
+    }
     
+    map.set(key,maxLen)
     
-    return max
-    
+    return maxLen
     
 }
