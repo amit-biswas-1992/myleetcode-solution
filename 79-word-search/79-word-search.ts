82bd79809f9a34f5e6247c1f7146da8d06211a6d
amit-biswas-1992 <78925979+amit-biswas-1992@users.noreply.github.com>
2022-07-27 09:47:05 +0600

    Time: 368 ms (94.50%), Space: 43.1 MB (96.22%) - LeetHub

--- 79-word-search/79-word-search.ts
+++ 79-word-search/79-word-search.ts
@@ -1,12 +1,13 @@
 //O(TC) = m*n*dfs() = m*n*4^w ,w=word length
 //O(SC) = m*n + dfs() = m*n + 4^w 
+//pattern: backtracking
 
 function exist(board: string[][], word: string): boolean {
-    let visited = new Array(board.length).fill(false).map( () => new Array(board[0].length).fill(false))
+    
     
     for(let i=0;i<board.length;i++){
         for(let j=0;j<board[0].length;j++){
-            if(board[i][j]==word[0] && wordSearch(board,word,0,i,j,visited)){
+            if(board[i][j]==word[0] && wordSearch(board,word,0,i,j)){
                 return true
             }
         }
@@ -15,16 +16,18 @@ function exist(board: string[][], word: string): boolean {
     return false
 };
 
-function wordSearch(board: string[][], word: string, index: number, i: number, j: number,  visited: boolean[][]){
-    if(i<0 || j<0 || i>=board.length || j>=board[0].length || visited[i][j] || board[i][j]!=word[index]) return false
+function wordSearch(board: string[][], word: string, index: number, i: number, j: number){
+    if(i<0 || j<0 || i>=board.length || j>=board[0].length || board[i][j]=='#' || board[i][j]!=word[index]) return false
     
-    visited[i][j] = true
     
     if(index == word.length-1 ) return true
     
-    let found = wordSearch(board,word,index+1, i+1,j,visited) || wordSearch(board,word,index+1,i-1,j,visited) || wordSearch(board,word,index+1,i,j+1,visited) || wordSearch(board,word,index+1,i,j-1,visited)
+    let char = board[i][j]
+    board[i][j] = '#'
     
-    visited[i][j] = false
+    let found = wordSearch(board,word,index+1, i+1,j) || wordSearch(board,word,index+1,i-1,j) || wordSearch(board,word,index+1,i,j+1) || wordSearch(board,word,index+1,i,j-1)
+    
+    board[i][j] = char
     
     return found
 
