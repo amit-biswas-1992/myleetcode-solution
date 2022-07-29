2af691d391043337e67ee04d6ebd44190291ca06
amit-biswas-1992 <78925979+amit-biswas-1992@users.noreply.github.com>
2022-07-29 18:00:14 +0600

    Time: 125 ms (61.19%), Space: 44.5 MB (54.10%) - LeetHub

--- 647-palindromic-substrings/647-palindromic-substrings.ts
+++ 647-palindromic-substrings/647-palindromic-substrings.ts
@@ -1,26 +1,27 @@
 function countSubstrings(s: string): number {
-    let count = 0
-    if( !s.length ) return 0
+    let count = 0;
     
     for(let i=0;i<s.length;i++){
-        count += palindomeSubstring(s,i,i)
-        count += palindomeSubstring(s,i,i+1)
+        count+=countPalindrome(s,i,i)
+        count+=countPalindrome(s,i,i+1)
     }
     
     return count
-    
-    
-    
 };
 
-function palindomeSubstring(s: string, left: number, right: number){
-    
+
+function countPalindrome(s:string,left:number,right:number){
     let count = 0
     
-    while(left>=0 && right<s.length && s[left]==s[right]){
-        count++
-        left--
-        right++
+    while(left>=0 || right<s.length){
+        if(s[left]==s[right]){
+            left--
+            right++
+            count++
+        }
+        else{
+            break
+        }
     }
     
     return count
