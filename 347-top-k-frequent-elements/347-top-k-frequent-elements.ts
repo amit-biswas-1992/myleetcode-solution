2cd5c9b577762c36d663b8d85e7dd737f25acc8e
amit-biswas-1992 <78925979+amit-biswas-1992@users.noreply.github.com>
2022-08-16 00:48:04 +0600

    Time: 134 ms (47.87%), Space: 50.5 MB (14.97%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.ts
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.ts
@@ -1,37 +1,39 @@
 function topKFrequent(nums: number[], k: number): number[] {
-    const freq = new Map<number,number>();
+    const n = nums.length
+    const freq = Array<number[]>(n+1)
+    const map: Map<number,number> = new Map();
     
-    const pq = new MinPriorityQueue({priority: ([key,frequ]) => frequ});
-    
-        
-        for(const num of nums){
-            if(freq.get(num))
-                freq.set(num,freq.get(num)+1)
-            else
-                freq.set(num,1)
-        }
-        
-        for(const [key,frequency] of freq){
-        
-            pq.enqueue([key,frequency]);
+    for(const num of nums){
+        if(!map.get(num))
+            map.set(num,0)
             
-            if(pq.size()>k){
-                pq.dequeue();
+        map.set(num,map.get(num)+1)
+    }
+    
+    for(let i = 0; i < freq.length; i++) { 
+        freq[i] = [];
+    }
+    
+    
+    
+    for( const [key,frequency] of map.entries()){
+        freq[frequency].push(key)
+    }
+    
 
+    
+    let taken = 0
+    let res = []
+    for(let i=n;i>=1;i--){
+        if(freq[i].length!=0){
+
+            for(const num of freq[i]){
+                res[taken++] = num
+                if(taken == k ) return res
             }
-
-            
         }
-        
-        let res = Array(k).fill(0);
-        
-        let size = pq.size();
-        
-        for(let i=0;i<size;i++){
-            let [key] = pq.dequeue().element;
-            res[size-i-1]=key
-        }
-        
-        return res;
+    }
+    
+  
 };
 
