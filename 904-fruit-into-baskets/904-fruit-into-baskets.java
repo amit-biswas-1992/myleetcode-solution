bea5d54c38c4dba0cd1e3616d86ca4df9d99b912
amit-biswas-1992 <78925979+amit-biswas-1992@users.noreply.github.com>
2022-08-16 11:07:49 +0600

    Time: 114 ms (25.61%), Space: 109.7 MB (40.62%) - LeetHub

---
+++ 904-fruit-into-baskets/904-fruit-into-baskets.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int totalFruit(int[] fruits) {
+        
+        int n = fruits.length;
+        
+        int windowStart = 0;
+        int maxFruit = 0;
+        
+        Map<Integer,Integer> fruitFrequencyMap = new HashMap<>();
+        
+        
+        
+        for(int windowEnd = 0; windowEnd<n; windowEnd++){
+            
+            int fruit = fruits[windowEnd];
+            
+            fruitFrequencyMap.put(fruit,fruitFrequencyMap.getOrDefault(fruit,0)+1);
+            
+            while(fruitFrequencyMap.size()>2){
+                
+                int windowStartFruit = fruits[windowStart];
+                
+                windowStart++;
+                fruitFrequencyMap.put(windowStartFruit,fruitFrequencyMap.get(windowStartFruit)-1);
+                
+                if(fruitFrequencyMap.get(windowStartFruit) == 0)
+                    fruitFrequencyMap.remove(windowStartFruit);
+                
+                
+            }
+            
+            
+            maxFruit = Math.max(maxFruit,windowEnd-windowStart+1);
+            
+        }
+        
+        return maxFruit;
+    }
+}
