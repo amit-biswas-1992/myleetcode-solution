058b11f77a5a88f141967677a278046db3de1337
Amit Biswas <78925979+amit-biswas-1992@users.noreply.github.com>
2022-09-02 12:50:00 +0600

    Time: 249 ms (34.94%), Space: 54 MB (18.07%) - LeetHub

---
+++ 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.ts
@@ -0,0 +1,47 @@
+function longestIncreasingPath(matrix: number[][]): number {
+    let memoPath : Map<string,number> = new Map() 
+    
+    let max = 0
+    let visited : boolean[][] = Array(matrix.length).fill(false).map( x => Array(matrix[0].length).fill(false))
+    
+    for(let i=0;i<matrix.length;i++){
+        for(let j=0;j<matrix[0].length;j++){
+            let pathLength = dfs(matrix,i,j,-1,visited,memoPath)
+            max = Math.max(max,pathLength)
+        }
+    }
+    
+    
+    return max
+};
+
+
+function dfs(matrix: number[][], i: number, j: number, prev: number,  visited: boolean[][], memoPath : Map<string,number>){
+    
+    
+    
+    if(i<0 || j<0 || i==matrix.length  || j==matrix[0].length || matrix[i][j] <= prev || visited[i][j]) return 0
+    
+    
+    let max = 0
+    
+    let key = i + "," + j
+    
+    if(memoPath.has(key)) return memoPath.get(key)
+    
+    visited[i][j] = true
+    
+    max = Math.max( 
+        1 + dfs(matrix,i+1,j,matrix[i][j],visited,memoPath),
+        1 + dfs(matrix,i-1,j,matrix[i][j],visited,memoPath),
+        1 + dfs(matrix,i,j+1,matrix[i][j],visited,memoPath),
+        1 + dfs(matrix,i,j-1,matrix[i][j],visited,memoPath),
+    )
+    
+    memoPath.set(key,max)
+    
+    visited[i][j] = false
+    
+    
+    return max
+}
