9a03e1100f44350262120986d007837ec6b26f13
Amit Biswas <78925979+amit-biswas-1992@users.noreply.github.com>
2022-09-03 22:41:14 +0600

    Time: 498 ms (100.00%), Space: 93.1 MB (100.00%) - LeetHub

---
+++ 2398-maximum-number-of-robots-within-budget/2398-maximum-number-of-robots-within-budget.ts
@@ -0,0 +1,43 @@
+function maximumRobots(chargeTimes: number[], runningCosts: number[], budget: number): number {
+    
+    let windowStart = 0
+    
+    let currentBudget = 0
+    let maxRobots = 0
+    let runningCostsSum = 0
+    
+    let maxChargeTime = 0
+    
+    
+    let maxPriorityQueue = new MaxPriorityQueue({priority: ( [charge,index] ) => charge })
+    
+    for(let windowEnd=0;windowEnd<chargeTimes.length;windowEnd++){
+        //max(chargeTimes) + k * sum(runningCosts)
+        
+        maxPriorityQueue.enqueue([chargeTimes[windowEnd],windowEnd])
+        
+        maxChargeTime = maxPriorityQueue.front().element[0]
+        
+        runningCostsSum += runningCosts[windowEnd]
+        
+        let windowSize = windowEnd-windowStart+1
+        currentBudget = maxChargeTime +  windowSize* runningCostsSum
+        
+        
+        if(currentBudget<=budget){
+            
+            maxRobots = Math.max(maxRobots,windowSize)
+        }
+        else{
+            
+            runningCostsSum -= runningCosts[windowStart]
+            windowStart++
+            while(!maxPriorityQueue.isEmpty() && maxPriorityQueue.front().element[1]<windowStart) maxPriorityQueue.dequeue()
+            
+        }
+    }
+    
+    return maxRobots
+        
+
+};
