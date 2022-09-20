5b604888813eb24c8c8c1f9d800806be5ee8ec32
Amit Biswas <78925979+amit-biswas-1992@users.noreply.github.com>
2022-09-21 00:43:17 +0600

    Time: 390 ms (27.59%), Space: 65.4 MB (79.31%) - LeetHub

--- 310-minimum-height-trees/310-minimum-height-trees.ts
+++ 310-minimum-height-trees/310-minimum-height-trees.ts
@@ -1,6 +1,10 @@
 function findMinHeightTrees(n: number, edges: number[][]): number[] {
     
+    //here indegree will not be good idea, because I need to get also the neighbour of indegree size=1, where I can get from a graph with array of node Set to keep neighbour
+    //TC = O(n), SC = O(n)
     if (n === 1) return [0];
+    //neet to assign type of Set, other wise neighbour of graph[node] will be undefined
+
     let graph = Array(n).fill(null).map( x => new Set<number>())
     
     
