eb7cda935934976c00bcdf4c80d5a2d0f1c056d0
Amit Biswas <78925979+amit-biswas-1992@users.noreply.github.com>
2022-09-20 12:36:48 +0600

    Time: 162 ms (48.61%), Space: 58.1 MB (60.65%) - LeetHub

--- 41-first-missing-positive/41-first-missing-positive.ts
+++ 41-first-missing-positive/41-first-missing-positive.ts
@@ -1,27 +1,10 @@
 function firstMissingPositive(nums: number[]): number {
+    let set = new Set()
     let n = nums.length
+    for(let num of nums) set.add(num)
     
-    for(let i=0;i<n;i++){
-        if(nums[i]<=0) nums[i] = n + 1
-    }
-    
-    
-    
-    
-    
-    for(let i=0;i<n;i++){
-        let num = Math.abs(nums[i])
-        if(num>0 && num<=n){
-            
-        
-            if(nums[num-1]>0) nums[num-1] = -nums[num-1]
-        }
-        
-    }
-    
-    
-    for(let i=0;i<n;i++){
-        if(nums[i]>0) return i+1
+    for(let i=1;i<=n;i++){
+        if(!set.has(i)) return i
     }
     
     return n+1
