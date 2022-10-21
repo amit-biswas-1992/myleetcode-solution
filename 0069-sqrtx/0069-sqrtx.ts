6bcaa537ca3cccde4347bab2e269c5d29f1ca91e
Amit Biswas <78925979+amit-biswas-1992@users.noreply.github.com>
2022-10-21 17:12:00 +0600

    Time: 66 ms (99.31%), Space: 44.6 MB (76.27%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.ts
+++ 0069-sqrtx/0069-sqrtx.ts
@@ -1,7 +1,19 @@
 function mySqrt(x: number): number {
     
-    let i=1
-    while(i*i<=x) i++
+    let i=0, j=x
+    
+    while(i<=j){
+        
+        let mid = i + Math.floor((j-i)/2)
+        
+        if(mid*mid<x){
+            i=mid+1
+        }
+        else if(mid*mid>x) j=mid-1
+        else return mid
+    }
     
     return i-1
+    
+    
 };
