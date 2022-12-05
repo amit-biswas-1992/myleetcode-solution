91f0217035fed5fe8e65588b106dc326fa9ea063
Amit Biswas <78925979+amit-biswas-1992@users.noreply.github.com>
2022-12-06 00:50:42 +0600

    Time: 129 ms (53.85%), Space: 46.4 MB (46.15%) - LeetHub

---
+++ 1026-maximum-difference-between-node-and-ancestor/1026-maximum-difference-between-node-and-ancestor.ts
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function maxAncestorDiff(root: TreeNode | null): number {
+    
+    let max = 0
+
+    dfs(root.left,root.val,root.val)
+    dfs(root.right,root.val,root.val)
+
+    function dfs(root: TreeNode | null, value: number,value2: number){
+        if(root==null) return
+
+        max = Math.max(max, Math.abs(root.val-value),Math.abs(root.val-value2))
+        let maxParent = Math.max(root.val,value)
+        let minParent = Math.min(root.val,value2)
+        dfs(root.left,maxParent,minParent)
+        dfs(root.right,maxParent,minParent)
+    }
+
+    return max
+
+
+};
